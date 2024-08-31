4cbe3c4d649db3ce4cf039416a590ce609c66526
ishikawa shinya <manari526@gmail.com>
2024-09-01 00:47:10 +0900

    予約機能・お気に入り機能実装

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -6,92 +6,106 @@
 
 @section('content')
 <div class="mypage__content">
+    <!-- ログイン中のユーザー名表示 -->
     <div class="mypage__header">
-        <!-- ログイン中のユーザー名表示 -->
-        <p class="mypage__header-ttl">testさん</p>  
+        @if(Auth::check())
+            <p class="mypage__header-ttl">{{ \Auth::user()->name }}さん</p>
+        @endif
     </div>
 
     <div class="mypage__main">
         <div class="mypage__main-left">
             <div class="main-left__ttl">
-                <p class="mypage__main-ttl">予約状況</p>
+                <p class="main__ttl">予約状況</p>
             </div>
 
-            <div class="reservation__confirmation">
-                <div class="confirmation__header">
-                    <div class="confirmation__header-left">
-                        <div class="header__img">
-                            <img src="{{ asset('icon/clock.svg') }}" alt="時計">
+            @foreach($reservations as $reservation)
+                <div class="reservation__confirmation">
+                    <div class="confirmation__header">
+                        <div class="confirmation__header-left">
+                            <div class="header__img">
+                                <img src="{{ asset('icon/clock.svg') }}" alt="時計">
+                            </div>
+                            <div class="header-ttl">
+                                <p>予約{{ $loop->iteration }}</p>
+                            </div>
                         </div>
-                        <div class="header-ttl">
-                            <p>予約1</p>
-                        </div>
-                    </div>
 
-                    <div class="header__right">
-                        <div class="header__right-img">
-                            <button type="submit" class="reservation__form-btn">
-                                <img src="{{ asset('icon/batsu.svg') }}" alt="取り消し">
-                            </button>
-                            
+                        <div class="header__right">
+                            <form action="/delete" method='post' class="reservation__form">
+                                @csrf
+                                <input type="hidden" name="id" value="{{ $reservation->id }}">
+                                <div class="header__right-img">
+                                    <button type="submit" class="reservation__form-btn">
+                                        <img src="{{ asset('icon/batsu.svg') }}" alt="取り消し">
+                                    </button>
+                                </div>
+                            </form>
                         </div>
-                    </div>
                    
+                    </div>
+                    <table class="reservation__confirmation-table">
+                        <tr>
+                            <th class="reservation__confirmation-ttl">Shop</th>
+                            <td class="selected__shop">{{ $reservation->shop->name }}</td>
+                        </tr>
+                        <tr>
+                            <th class="reservation__confirmation-ttl">Date</th>
+                            <td class="selected__date">{{ $reservation->reservation_date }}</td>
+                        </tr>
+                        <tr>
+                            <th class="reservation__confirmation-ttl">Time</th>
+                            <td class="selected__time">{{ $reservation->reservation_time }}</td>
+                        </tr>
+                        <tr>
+                            <th class="reservation__confirmation-ttl">Number</th>
+                            <td class="selected__num">{{ $reservation->reservation_num }}</td>
+                        </tr>
+                    </table>
                 </div>
-                <table class="reservation__confirmation-table">
-                    <tr>
-                        <th class="reservation__confirmation-ttl">Shop</th>
-                        <td class="selected__shop">店名</td>
-                    </tr>
-                    <tr>
-                        <th class="reservation__confirmation-ttl">Date</th>
-                        <td class="selected__date">日付</td>
-                    </tr>
-                    <tr>
-                        <th class="reservation__confirmation-ttl">Time</th>
-                        <td class="selected__time">時間</td>
-                    </tr>
-                    <tr>
-                        <th class="reservation__confirmation-ttl">Number</th>
-                        <td class="selected__num">人数</td>
-                    </tr>
-                </table>
-            </div>
+            @endforeach
         </div>
 
         <div class="mypage__main-right">
-            <div class="main-left__ttl">
-                <p class="mypage__main-ttl">お気に入り店舗</p>
+            <div class="main-right__ttl">
+                <p class="main__ttl">お気に入り店舗</p>
             </div>
 
+            
             <div class="favolit__list">
                 <div class="wrapper">
-                    <div class="card">
-                        <div class="shop__img">
-                            <img src="https://coachtech-matter.s3-ap-northeast-1.amazonaws.com/image/sushi.jpg" alt="画像がありません">
-                        </div>
+                    @foreach($favorites as $favorite)
+                        <div class="card">
+                            <div class="shop__img">
+                                <img src="{{ $favorite->shop->image_url }}" alt="{{ $favorite->shop->name }}">
+                            </div>
 
-                        <div class="card__item">
-                            <div class="shop__name">
-                                <p>店名</p>
+                            <div class="card__item">
+                                <div class="shop__name">
+                                    <p>{{ $favorite->shop->name  }}</p>
+                                </div>
+                                <div class="text__box">
+                                    <p class="area">#{{ $favorite->shop->area->name }}</p>
+                                    <p class="genre">#{{ $favorite->shop->genre->name }}</p>
+                                </div>
                             </div>
-                            <div class="text__box">
-                                <p class="area">#エリア</p>
-                                <p class="genre">#ジャンル</p>
-                            </div>
-                        </div>
         
-                        <div class="card__btn">
-                            <div class="detail__link">
-                                <a href="" class="detail__link-btn">詳しく見る</a>
-                            </div>
-                            <div class="favorit__form">
-                                <button type="submit" class="favorit__form-btn">
-                                    <img src="{{ asset('icon/heart.svg') }}" alt="お気に入り" class="heart-icon">
-                                </button>
+                            <div class="card__btn">
+                                <div class="detail__link">
+                                    <a href="/detail/{{ $favorite->shop->id }}" class="detail__link-btn">詳しく見る</a>
+                                </div>
+                                <div class="shop__favorit">
+                                    <form action="{{ route('favorites.destroy', $favorite->shop->id) }}" method="POST" class="shop__favorit-form">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="favorit__form-btn">
+                                            <img src="{{ asset('icon/heart_color.svg') }}" alt="お気に入り解除" class="heart-icon">
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
